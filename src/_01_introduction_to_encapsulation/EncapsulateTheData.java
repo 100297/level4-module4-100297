e0ad5152ea8c0a633eb937b0f67def65beea8452
SomeRandomKid <league@iMac.attlocal.net>
2018-12-16 12:56:19 -0800

    aching vkcastey grwtuke fg
    df
    ip has[puHDAs[usdhA[us
    ASPl d
    asd
    [asojd
    as[da
    sokd
    a[osdk
    a[skod
    a[sokd
    [aok
    [ok
    [ok
    [ok
    [ok
    ok
    [ok
    [ok
    [ok
    [ok
    [ok
    [ok
    [ok[ok[ok[ok[ko[k[ok[ok[ok[ok
    [ok
    [ok[ok
    [ok[ok
    [ok
    [ok
    [ok
    [ok
    [ok
    [ok
    [ok
    [ok

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -15,20 +15,57 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj; //can be any object type except String. Strings get turned into objects.
-	
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; // can be any object type except String. Strings get turned into objects.
+
 	public static void main(String[] args) {
-		
+
+	}
+
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+
+	public void setItemsReceived(int itemsReceived) {
+
+		this.itemsReceived = itemsReceived;
+		if (this.itemsReceived < 0) {
+			itemsReceived = 0;
+		}
+	}
+
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+
+	public void setDegreesTurned(float degreesTurned) {
+		this.degreesTurned = degreesTurned;
+		if (this.degreesTurned < 0) {
+			degreesTurned = 0;
+		}
+		if (this.degreesTurned > 360) {
+			degreesTurned = 360;
+		}
+	}
+
+	public String getNomenclature() {
+		return nomenclature;
+	}
+
+	public void setNomenclature(String nomenclature) {
+		this.nomenclature = nomenclature;
+		if (this.nomenclature.equals("")) {
+				nomenclature = " ";
+		}
 	}
 }
